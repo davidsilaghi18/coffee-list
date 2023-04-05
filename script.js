3097469b48a031519bb15c7be41ff7103b6695cd
David Silaghi <118262626+davidsilaghi18@users.noreply.github.com>
2023-04-05 12:51:56 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const menuButton = document.querySelector('.nav-item:first-child');
+menuButton.addEventListener('click', () => {
+  alert('The best coffee');
+});
+
+const dkButton = document.querySelector('.nav-item:last-child');
+dkButton.addEventListener('click', () => {
+  alert('Hi, I\'m David and I\'m funny');
+});
